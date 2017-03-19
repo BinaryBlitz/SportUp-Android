279c3556430e48518d9a472f5ced7f5961e1f502
outboard <efanovev1995@gmail.com>
2017-03-19 19:30:26 +0300

    Add activity class

---
+++ app/src/main/java/ru/binaryblitz/sportup/activities/StartActivity.java
@@ -0,0 +1,28 @@
+package ru.binaryblitz.sportup.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import ru.binaryblitz.sportup.R;
+import ru.binaryblitz.sportup.base.BaseActivity;
+
+public class StartActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        findViewById(R.id.start_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, SelectLocationActivity.class));
+                finish();
+            }
+        });
+    }
+
+}
+
