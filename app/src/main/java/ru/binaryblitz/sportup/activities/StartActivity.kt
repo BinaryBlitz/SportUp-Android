a83bfd3023731199c11407e407a9ea5d4436809d
outboard <efanovev1995@gmail.com>
2017-03-21 19:31:50 +0300

    Add Fabric

--- app/src/main/java/ru/binaryblitz/sportup/activities/StartActivity.kt
+++ app/src/main/java/ru/binaryblitz/sportup/activities/StartActivity.kt
@@ -3,6 +3,8 @@ package ru.binaryblitz.sportup.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 import ru.binaryblitz.sportup.R
 import ru.binaryblitz.sportup.base.BaseActivity
@@ -12,6 +14,7 @@ class StartActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Fabric.with(this, Crashlytics())
 
         findViewById(R.id.start_btn).setOnClickListener {
             startActivity(Intent(this@StartActivity, SelectCityActivity::class.java))
