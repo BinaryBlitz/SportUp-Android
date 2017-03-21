dc4f8aa9463e577dc8d2f3216e82228a890b85cc
outboard <efanovev1995@gmail.com>
2017-03-21 16:56:59 +0300

    Add tabbar activity

--- app/src/main/java/ru/binaryblitz/sportup/activities/SportsActivity.kt
+++ app/src/main/java/ru/binaryblitz/sportup/activities/SportsActivity.kt
@@ -1,7 +1,19 @@
 package ru.binaryblitz.sportup.activities
 
+import android.os.Bundle
 import ru.binaryblitz.sportup.base.BaseActivity
-import ru.binaryblitz.sportup.utils.LocationManager
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.binaryblitz.sportup.R
+
 
 class SportsActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomBar.setOnTabSelectListener { tabId ->
+//            if (tabId == R.id.tab_favorites) {
+//            }
+        }
+    }
 }
