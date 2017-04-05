e94cb313788a7a6cf65e0401a8435bf5c8b7eeab
outboard <efanovev1995@gmail.com>
2017-04-05 13:27:35 +0300

    Add button for event creation

--- app/src/main/java/ru/binaryblitz/SportUp/fragments/UserEventsFragment.kt
+++ app/src/main/java/ru/binaryblitz/SportUp/fragments/UserEventsFragment.kt
@@ -1,5 +1,6 @@
 package ru.binaryblitz.SportUp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_user_events.*
 import ru.binaryblitz.SportUp.R
+import ru.binaryblitz.SportUp.activities.CreateEventActivity
 import ru.binaryblitz.SportUp.adapters.MyEventsAdapter
 import ru.binaryblitz.SportUp.base.BaseFragment
 import ru.binaryblitz.SportUp.presenters.MyEventsPresenter
@@ -31,6 +33,11 @@ class UserEventsFragment : BaseFragment() {
 
         initList()
         load()
+
+        rightBtn.setOnClickListener {
+            val intent = Intent(activity, CreateEventActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun initList() {
