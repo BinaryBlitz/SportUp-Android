533aef38920d236f06e1dc2cf23503bf3ca7dc67
outboard <efanovev1995@gmail.com>
2017-04-11 15:17:26 +0300

    Add UI and adapter

---
+++ app/src/main/java/ru/binaryblitz/SportUp/activities/VotesActivity.kt
@@ -0,0 +1,63 @@
+package ru.binaryblitz.SportUp.activities
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Pair
+import kotlinx.android.synthetic.main.activity_players_list.*
+import ru.binaryblitz.SportUp.R
+import ru.binaryblitz.SportUp.adapters.VotesAdapter
+import ru.binaryblitz.SportUp.base.BaseActivity
+import ru.binaryblitz.SportUp.server.EndpointsService
+import ru.binaryblitz.SportUp.utils.AndroidUtilities
+import javax.inject.Inject
+
+class VotesActivity : BaseActivity() {
+    val EXTRA_COLOR = "color"
+    val EXTRA_ID = "id"
+    val DEFAULT_COLOR = Color.parseColor("#212121")
+
+    private lateinit var adapter: VotesAdapter
+
+    @Inject
+    lateinit var api: EndpointsService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_votes)
+
+        initToolbar()
+        initList()
+        setOnClickListeners()
+    }
+
+    private fun initToolbar() {
+        val color = intent.getIntExtra(EXTRA_COLOR, DEFAULT_COLOR)
+        appBarView.setBackgroundColor(color)
+        AndroidUtilities.colorAndroidBar(this, color)
+    }
+
+    fun initList() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.setHasFixedSize(true)
+
+        adapter = VotesAdapter(this)
+        recyclerView.adapter = adapter
+    }
+
+    private fun setOnClickListeners() {
+        backBtn.setOnClickListener { finish() }
+    }
+
+    fun onLoaded(collection: ArrayList<Pair<String, Any>>) {
+
+    }
+
+    private fun load() {
+
+    }
+
+}
+
---
+++ app/src/main/java/ru/binaryblitz/SportUp/adapters/VotesAdapter.kt
@@ -0,0 +1,55 @@
+package ru.binaryblitz.SportUp.adapters
+
+import android.app.Activity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import ru.binaryblitz.SportUp.R
+import ru.binaryblitz.SportUp.models.Player
+import ru.binaryblitz.SportUp.utils.Image
+import java.util.*
+
+class VotesAdapter(private val context: Activity) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    val EXTRA_ID = "id"
+    val EXTRA_COLOR = "color"
+
+    private var players: ArrayList<Player>
+
+    init {
+        players = ArrayList<Player>()
+    }
+
+    fun setCollection(players: ArrayList<Player>) {
+        this.players = players
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_vote, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        val holder = viewHolder as ViewHolder
+
+        val player = players[position]
+
+        holder.name.text = player.name
+        Image.loadAvatar(context, player.name, player.avatarUrl, holder.avatar)
+
+        holder.itemView.setOnClickListener {
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return players.size
+    }
+
+    private inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var name = itemView.findViewById(R.id.name) as TextView
+        internal var avatar = itemView.findViewById(R.id.avatar) as ImageView
+    }
+}
